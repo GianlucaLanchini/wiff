9b53ecd7f6492b1a9a48a79656396f1942b3d0d3
GianlucaLanchiniUnicam <gianluca.lanchini@studenti.unicam.it>
2022-11-16 16:32:06 +0100

    Primo Commit
    Combinati i progetti e unite le visualizzazioni. Da una parte gzweb che già funziona con fame e dall'altra bpmnjs in cui puoi caricare via input button un diagramma bpmn da visualizzare (funziona solo in modo isolato)

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# wiff
+# wiff - Web Interface for FaMe
+
+Web application built combining technologies from both gzweb (https://github.com/osrf/gzweb), for the 3d visualization of the running fame simulation, and bpmn-js-token-simulation (https://github.com/bpmn-io/bpmn-js-token-simulation), for the visualization of the BPMN diagram and of its running tokens.
+
+(Working with Node.js 16.18.0)
+
+## Build and Run
+
+Build the gzweb part (you need to first install the dependencies at http://classic.gazebosim.org/tutorials?tut=gzweb_install&cat=gzweb):
+
+(Warning: it'll download all the gazebo models locally, it may take some minutes)
+
+```sh
+source /usr/share/gazebo/setup.sh
+npm run deploy --- -m
+```
+
+Then build the bpmnjs part:
+
+```sh
+npm run bundle
+```
+
+To run the application you first need to launch gzserver on a second terminal:
+
+```sh
+gzserver
+```
+
+Then start the application with:
+
+```sh
+npm start
+```
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+/* eslint-env node */
+
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { DefinePlugin, ProvidePlugin } = require('webpack');
+
+
+module.exports = (env, argv) => {
+
+  const mode = argv.mode || 'development';
+
+  const devtool = mode === 'development' ? 'eval-source-map' : 'source-map';
+
+  return {
+    mode,
+    entry: {
+      viewer: './gz3d/bpmn-js/viewer.js',
+      modeler: './gz3d/bpmn-js/modeler.js'
+    },
+    output: {
+      filename: 'dist/[name].js',
+      path: __dirname + '/http/client'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.bpmn$/,
+          type: 'asset/source'
+        }
+      ]
+    },
+    plugins: [
+      new CopyWebpackPlugin({
+        patterns: [
+          { from: './assets', to: 'dist/vendor/bpmn-js-token-simulation/assets' },
+          { from: 'bpmn-js/dist/assets', context: 'node_modules', to: 'dist/vendor/bpmn-js/assets' },
+          { from: 'bpmn-js-properties-panel/dist/assets', context: 'node_modules', to: 'dist/vendor/bpmn-js-properties-panel/assets' }
+        ]
+      }),
+      new DefinePlugin({
+        'process.env.TOKEN_SIMULATION_VERSION': JSON.stringify(require('./package.json').version)
+      })
+    ],
+    devtool
+  };
+
+};
